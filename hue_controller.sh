f7244afe5fe5d1f405eeb94b01af6109b38a706f
mneveu <maxime.nv@orange.fr>
2019-08-06 16:09:55 +0200

    Ajout gestion sensors et initialisation controller

--- hue_controller.sh
+++ hue_controller.sh
@@ -5,13 +5,16 @@ set -o pipefail
 ##### VARIABLES ######
 CONFIG_FILE="hue_controller.yml"
 MANDATORY_PARAMETERS=( conf_huebridge_ip conf_sensor_name conf_light_name conf_debug conf_hue_api_token conf_store_objects_in_files )
-FILE_INDEX_LIGHTS="datas/index_lights.mx"
-FILE_INDEX_SENSORS="datas/index_sensors.mx"
+PATH_DATAS="datas"
+PATH_STORE_LIGHTS=$PATH_DATAS"/lights"
+PATH_STORE_SENSORS=$PATH_DATAS"/sensors"
+FILE_INDEX_LIGHTS=$PATH_DATAS"/index_lights.mx"
+FILE_INDEX_SENSORS=$PATH_DATAS"/index_sensors.mx"
 
 ######################
 
 
-#Parse YAML : @ pkuczynski/parse_yaml.sh
+#Parse YAML : @pkuczynski/parse_yaml.sh
 parse_yaml() {
    local prefix=$2
    local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
@@ -41,19 +44,39 @@ check_file_is_updated() {
 
 get_index_lights() {
 	#curl $HUE_API_URL"/"$1 \
-	cat example.json \
-	| jq -r '.lights | keys[] as $k | "\"\(.[$k].name)\"=\($k)"' 1>$1 || (echo "L'index $(basename $1) n'a pas été correctement récupéré ou parsé" && exit 1)
+	cat example_lights.json \
+	| jq -r '.lights | keys[] as $k | "\(.[$k].name)=\($k)"' 1>$1 || (echo "L'index $(basename $1) n'a pas été correctement récupéré ou parsé" && exit 1)
 	check_file_is_updated $1 2 
 }
 
 
 get_index_sensors() {
 	#curl $HUE_API_URL"/"$1 \
-	cat example.json \
-	| jq -r '.sensors | keys[] as $k | "\"\(.[$k].name)\"=\($k)"' 1>$1 || (echo "L'index $(basename $1) n'a pas été correctement récupéré ou parsé" && exit 1)
+	cat example_sensors.json \
+	| jq -r '.sensors | keys[] as $k | "\(.[$k].name)=\($k)"' 1>$1 || (echo "L'index $(basename $1) n'a pas été correctement récupéré ou parsé" && exit 1)
 	check_file_is_updated $1 2
 }
 
+create_status_file() {
+	IFS="="
+	while read -r k v
+	do
+		cat $1 | jq ".$2.\"$v\"" > $3"/"$k
+	done < $4
+	test $conf_debug -eq 1 && echo $(ls $3 | wc -l)" created file(s) for "$2
+}
+
+clean_status_files() {
+	rm -rf $1"/*"
+}
+
+get_json_value() {
+	cat $1 | jq $2
+}
+
+get_json_sensor_value() {
+	get_json_value $PATH_STORE_SENSORS"/"$conf_sensor_name $1
+}
 echo "Hue controller @MNE_v0.1 : August 2018"
 
 echo "Lecture du fichier de configuration : $CONFIG_FILE"
@@ -67,18 +90,21 @@ done
 ## Paramètres HUE ##
 HUE_API_URL="http://"$conf_huebridge_ip"/api/"$conf_hue_api_token"/"
 get_index_lights $FILE_INDEX_LIGHTS
-#get_index_sensors $FILE_INDEX_SENSORS
+get_index_sensors $FILE_INDEX_SENSORS
 
 test $conf_debug -eq 1 && echo $(cat $FILE_INDEX_LIGHTS);
 test $conf_debug -eq 1 && echo $(cat $FILE_INDEX_SENSORS);
-for i in ${INDEX_LIGHTS[@]}; do echo $i; done
 
 echo "Créer 1 fichier statut par équipement Hue"
+clean_status_files $PATH_STORE_LIGHTS
+create_status_file example_lights.json "lights" $PATH_STORE_LIGHTS $FILE_INDEX_LIGHTS
 
+clean_status_files $PATH_STORE_SENSORS
+create_status_file example_sensors.json "sensors" $PATH_STORE_SENSORS $FILE_INDEX_SENSORS
 
-echo "Récupération de l'état des lumières"
-#state_lights_json=$(curl $HUE_API_URL"/lights");
+echo "Controller"
+test $conf_debug -eq 1 && echo "\""$conf_light_name"\" controlled by \""$conf_sensor_name"\""
 
-#echo $state_lights_json
-
-echo "Récupération de l'état des détecteurs"
+echo "Vérification état sensor"
+SENSOR_ON=$(get_json_sensor_value ".config.on")
+SENSOR_REACHABLE=$(get_json_sensor_value ".config.reachable")
